14aa4a67e438812e33a18470fd14748d48a67214
hypercurious <adonispappas9@gmail.com>
2022-07-27 01:30:09 +0300

    get all products along with the last product;get the right availability/brand of the products

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import json
 import pandas
 import requests
 from bs4 import BeautifulSoup
@@ -8,7 +9,7 @@ def main(link, filename):
     get_request = requests.get(link)
     
     scraping = BeautifulSoup(get_request.text, 'html.parser')
-    products = [product for product in scraping.find_all(class_='column item')]
+    products = [product for product in scraping.find(class_='product-list product-list product-list--default')]
 
     # Get the data
     urls = []
@@ -16,46 +17,55 @@ def main(link, filename):
     skus = []
     initial_prices = []
     final_prices = []
-    availabilities = [] # all availabilities are 'Εξαντλήθηκε', because the website loads the value 'Εξαντλήθηκε' for all products and after a while it loads the correct value
     for product in products:
-        urls.append('https://plaisio.gr'+product.find('a').get('href'))
+        url = product.find('a').get('href')
+        domain = '' if url.startswith('https://www.plaisio.gr') else 'https://www.plaisio.gr'
+        urls.append(domain+url)
         names.append(product.find(class_='product-title').text)
         skus.append(product.find(class_='sku').text)
-        initial_prices.append(product.find(class_='price').find(class_='from-price').text)
-        final_prices.append(product.find(class_='price').find(class_='price').text)
-        availabilities.append(product.find(class_='stock-indication-text').text)
-
-    brands = [name.split(' ')[0] for name in names]
+        try:
+            initial_price = "%.2f" % float(product.find(class_='price').find(class_='from-price').text)
+        except ValueError:
+            initial_price = ""
+        initial_prices.append(initial_price)
+        final_prices.append("%.2f" % float(product.find(class_='price').find(class_='price').text))
 
     redirect_requests = [requests.get(url) for url in urls]
     redirect_scrapings = [BeautifulSoup(redirect_request.text, 'html.parser') for redirect_request in redirect_requests]
 
-    scripts = [redirect_scraping.find(class_='pdp-imagelist').script.text for redirect_scraping in redirect_scrapings]
-    product_eans_re = [re.search('"productEan":"([0-9]*)"', script) for script in scripts]
+    scripts = [json.loads(redirect_scraping.find('script', type='application/ld+json').text) for redirect_scraping in redirect_scrapings]
+    brands = [brand['brand']['name'] for brand in scripts]
+    availabilities = [availability['offers']['availability'].partition('schema.org/')[2] for availability in scripts]
+    
+    ean_scripts = [redirect_scraping.find(class_='pdp-imagelist').script.text for redirect_scraping in redirect_scrapings]
+    product_eans_re = [re.search('"productEan":"([0-9]*)"', script) for script in ean_scripts]
     product_eans = [x.group(1) if x else 'None' for x in product_eans_re]
 
-    df = pandas.DataFrame()
-    df['url'] = urls
-    df['ProductName'] = names
-    df['Sku'] = skus
-    df['Brand'] = brands
-    df['Final Price'] = final_prices
-    df['Availability'] = availabilities
-    df['Initial Price'] = initial_prices
-    df['Ean'] = product_eans
+    df = pandas.DataFrame(
+        {
+            'url': urls,
+            'ProductName': names,
+            'Sku': skus,
+            'Brand': brands,
+            'Final Price': final_prices,
+            'Availability': availabilities,
+            'Initial Price': initial_prices,
+            'Ean': product_eans
+        }
+    )
     df.to_csv(filename)
 
 if __name__=='__main__':
     links = [
         'https://www.plaisio.gr/pc-perifereiaka/laptops/windows-laptops',
         'https://www.plaisio.gr/anavathmisi-diktia/anavathmisi-pc/koutia-desktop/',
-        'https://www.plaisio.gr/smart-tech-gadgets/wearables/smartwatch'
+        'https://www.plaisio.gr/smart-tech-gadgets/wearables/smartwatch',
     ]
     filenames = [
         'products.csv',
         'desktop_cases.csv',
-        'smartwatches.csv'
+        'smartwatches.csv',
     ]
     for link, filename in zip(links, filenames):
         main(link, filename)
-        print('Done')
+        print(f'{filename} is ready')
